fe6be288e7763ef76f4fb615c7a62e0083780499
owner <owner@DESKTOP-3252VA6>
2019-12-10 20:17:43 +0900

    호진 카트 컨트롤러 아이디 세션 체크 수정

--- team1_user_prj3/src/kr/co/jinibooks/controller/cartController.java
+++ team1_user_prj3/src/kr/co/jinibooks/controller/cartController.java
@@ -5,6 +5,8 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,14 +22,31 @@ import kr.co.jinibooks.vo.SearchIdVO;
 @Controller
 public class cartController {
 	
+	/**
+	 * 로그인 여부 확인
+	 * @return
+	 */
+	private String ChkLoginSession(HttpSession session) {
+		String destination = null;
+		
+		if((String)session.getAttribute("user_id") == null) {
+			destination = "redirect:/member/login.do";
+		}//end if
+		return destination;
+	}//ChkLoginSession
+	
 	//장바구니 이동
 	@RequestMapping(value = "book_cartList.do", method = GET)
-	public String bookCartProcess( Model model,SearchIdVO siVO) {  //
-		SearchCartService scs= new SearchCartService();
-		List<SearchCartListDomain> cartList=scs.selectCartList(siVO);
-		model.addAttribute("cartList", cartList);
-		return "book_cart";
-	}//AddCartProcess
+	public String bookCartProcess(HttpSession session, Model model, SearchIdVO siVO) {  //
+		String destination = ChkLoginSession(session);
+		if(destination == null) {
+			SearchCartService scs= new SearchCartService();
+			List<SearchCartListDomain> cartList=scs.selectCartList(siVO);
+			model.addAttribute("cartList", cartList);
+		    destination = "book_cart";
+	    }//end if
+	    return destination;
+	}//bookCartProcess
 	
 	//장바구니  추가
 	@RequestMapping(value="add_cart.do", method = GET)
